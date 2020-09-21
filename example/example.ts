71335abe3dfadd812b159a3bb496428b7c9468f6
Vahid V <vhid.vz@gmail.com>
2020-09-21 11:06:50 +0330

    An example was added.

--- example/example.ts
+++ example/example.ts
@@ -1,17 +1,3 @@
-# workflow-lib
-
-Workflow engine based on BPMNs. Designed for developers.
-
-[docs](https://wenexo.github.io/workflow-lib/)
-
-## Example
-
-![diagram](./example/diagram.svg)
-
-Before use this xml diagram you should convert it to a plain json format.
-May you want to use [freeformatter](https://www.freeformatter.com/xml-to-json-converter.html), don't forget to change prefix to underline ( @ -> \_ ).
-
-```ts
 import { BPMNEventActivity, BPMNGatewayActivity } from 'activities';
 import * as diagram from './diagram.process.json';
 import { BPMNInterface, BPMNEngine } from 'index';
@@ -69,16 +55,3 @@ engine
 
 const serializedEngine = engine.serialize();
 // Save `serializedEngine` json model to your db
-```
-
-### Note:
-
-- All methods that are not implemented will automatically done with preferred options.
-- Methods with `_` at starting point of name was automatically called.
-- When engine found a normal method will stoped, then to you call `run` again to continue.
-
-**To start example:**
-
-```sh
-npm run example
-```
